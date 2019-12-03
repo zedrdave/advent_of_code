d16fc3de7ba55d8cd18fee820fbccad099a752c4
Dave duVerle <dave.duverle@novadiscovery.com>
2019-12-03 10:55:03 +0100

    code clean-up

--- 3/3_2.py
+++ 3/3_2.py
@@ -1,6 +1,7 @@
-lines = [l.strip().split(',') for l in open("input.txt", "r").readlines()]
+lines = [l.strip().split(',') for l in open("input_test0.txt", "r").readlines()]
 paths = [[(w[0], int(w[1:])) for w in l] for l in lines]
 
+# Wrapper to avoid dealing with confusing p[0]/p[1] all the time:
 class Point:
     def __init__(self, x, y):
         self.x = x
@@ -8,7 +9,7 @@ class Point:
     def __repr__(self):
         return f"({self.x},{self.y})"
 
-def path_to_segments(path):
+def PathToSegments(path):
     segments = []
     cur_pos = Point(0,0)
     for w in path:
@@ -27,26 +28,24 @@ def path_to_segments(path):
         cur_pos = next_pos
     return segments
 
-(ls1, ls2) = [path_to_segments(p) for p in paths]
+# easier to store and deal with segments than points:
+(ls1, ls2) = [PathToSegments(p) for p in paths]
 
-def is_vert(s):
-    return s[0].x == s[1].x
+isVertical = lambda s: s[0].x == s[1].x
 
-def segs_intersect(s1, s2):
-    if is_vert(s1) == is_vert(s2):
+def SegmentsIntersect(s1, s2):
+    # only considering perpendicular intersections:
+    if isVertical(s1) == isVertical(s2):
         return False
-    # assume s1 is horiz and s2 is vertical and both positives:
-    if is_vert(s1):
+    # WLOG: s1 horiz, s2 vertical, and both positives:
+    if isVertical(s1):
         s1, s2 = s2, s1
     if s1[0].x > s1[1].x:
         s1 = [s1[1],s1[0]]
     if s2[0].y > s2[1].y:
         s2 = [s2[1],s2[0]]
-    if s1[0].x < s2[0].x and s1[1].x > s2[0].x and \
-        s1[0].y > s2[0].y and  s1[0].y < s2[1].y:
-        return Point(s2[0].x, s1[0].y)
-    else:
-        return False
+    # check for intersection
+    return s1[0].x < s2[0].x < s1[1].x and s2[0].y < s1[0].y < s2[1].y and Point(s2[0].x, s1[0].y)
 
 def L1Dist(p1, p2):
     return abs(p2.x - p1.x) + abs(p2.y - p1.y)
@@ -58,15 +57,12 @@ s1_steps = 0
 for s1 in ls1:
     s2_steps = 0
     for s2 in ls2:
-        print(f"{s1} {s2}")
-        inter = segs_intersect(s1, s2)
+        inter = SegmentsIntersect(s1, s2)
         if inter:
-            print(f"*** Intersect: {inter}")
             intersections += [inter]
             step_counts += [s1_steps + s2_steps + L1Dist(s1[0], inter) + L1Dist(s2[0], inter)]
         s2_steps += L1Dist(*s2)
     s1_steps += L1Dist(*s1)
-
 
 print("Intersections: ", intersections)
 dists = [abs(p.x)+abs(p.y) for p in intersections]
