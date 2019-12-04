a47639b21a34a9612e8ea12e68b4bebc74ec2875
Dave <dave@unknowngenius.com>
2019-12-04 07:42:19 +0100

    quickndirty

---
+++ 04/4_2.py
@@ -0,0 +1,17 @@
+import numpy as np
+xmin = 273025
+xmax = 767253
+
+count = 0
+for x in range(xmin, xmax+1):
+    s = [int(d) for d in str(x)]
+    s.insert(0, 0)
+    s = np.array(s)
+    s = np.sign(s[1:] - s[:-1])
+    if any(s < 0) or all(s > 0):
+        continue
+    s2 = np.append(s, 1)
+    for i in range(0, len(s2-3)):
+        if np.array_equal(s2[i:(i+3)], [1,0,1]):
+            count += 1
+            break
