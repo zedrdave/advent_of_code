74fd8a7a990c57be9d1b72c92ace58188ae25b63
Dave <dave@unknowngenius.com>
2019-12-08 23:27:35 +0100

    clean up

--- 08/8.py
+++ 08/8.py
@@ -9,13 +9,14 @@ layers = np.array(digits).reshape((-1,size[0]*size[1]))
 # Part 1
 zeros = np.apply_along_axis(lambda l: sum(l == 0), 1, layers)
 min0 = np.argmin(zeros)
-print(sum(layers[min0] == 1) * sum(layers[min0] == 2))
+print("Part 1: ", sum(layers[min0] == 1) * sum(layers[min0] == 2))
 
 # Part 2
 layers = np.array(digits).reshape((-1,*size))
 composite = np.apply_along_axis(lambda x: x[np.where(x != 2)[0][0]], axis=0, arr=layers)
 
 # ASCII art version:
+print("Part 2:")
 print("\n".join(''.join(u" ♥️"[int(i)] for i in line) for line in composite))
 
 # Optional: using Pillow
