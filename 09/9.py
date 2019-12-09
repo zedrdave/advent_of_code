837c837376d766335054c55b42a733f970950991
Dave duVerle <dave.duverle@novadiscovery.com>
2019-12-09 16:49:23 +0100

    clean up

--- 09/9.py
+++ 09/9.py
@@ -1,9 +1,21 @@
-import sys
 import copy
 import itertools
 import collections
+from enum import IntEnum
 
-class Machine:
+class OP(IntEnum):
+    ADD = 1
+    MULT = 2
+    INPUT = 3
+    OUTPUT = 4
+    JUMP_IF_TRUE = 5
+    JUMP_IF_FALSE = 6
+    LESS_THAN = 7
+    EQUALS = 8
+    INCREMENT_RB = 9
+    HALT = 99
+
+class VM:
     def __init__(self, program, phase = None):
         self.mem = collections.defaultdict(int)
         for i,d in enumerate(program):
@@ -15,43 +27,42 @@ class Machine:
             self.IP += 2
 
     def run(self, input):
-        while self.mem[self.IP] != 99:
+        while self.mem[self.IP] != OP.HALT:
             # print(f"[{self.IP}] {self.mem[self.IP]}: {self.mem[self.IP+1]} {self.mem[self.IP+2]} {self.mem[self.IP+3]}")
 
             cmd = self.mem[self.IP] % 100
 
-            if cmd == 1: # op_add
+            if cmd == OP.ADD:
                 self.set_param(3, self.param(1) + self.param(2))
                 self.IP += 4
-            elif cmd == 2: # op_mult
+            elif cmd == OP.MULT:
                 self.set_param(3, self.param(1) * self.param(2))
                 self.IP += 4
-            elif cmd == 3: # op_input
+            elif cmd == OP.INPUT:
                 print(f"using input: {input}")
                 self.set_param(1, input)
                 self.IP += 2
-            elif cmd == 4: # op_output
+            elif cmd == OP.OUTPUT:
                 output = self.param(1)
                 print(f"output: {output}\n")
                 self.IP += 2
                 # return output
-            elif cmd == 5: # op_jump_if_true
+            elif cmd == OP.JUMP_IF_TRUE:
                 self.IP = self.param(2) if self.param(1) else self.IP+3
-            elif cmd == 6: # op_jump_if_false
+            elif cmd == OP.JUMP_IF_FALSE:
                 self.IP = self.param(2) if not self.param(1) else self.IP+3
-            elif cmd == 7: # op_less_than
+            elif cmd == OP.LESS_THAN:
                 self.set_param(3, self.param(1) < self.param(2))
                 self.IP += 4
-            elif cmd == 8: # op_equals
+            elif cmd == OP.EQUALS:
                 self.set_param(3, self.param(1) == self.param(2))
                 self.IP += 4
-            elif cmd == 9: # op_adjust_RB
+            elif cmd == OP.INCREMENT_RB:
                 self.RB += self.param(1)
                 # print(f"RB: {self.RB}")
                 self.IP += 2
             else: # op_err
-                print(f"Unknown opcode: {self.mem[self.IP]} ")
-                sys.exit(-1)
+                raise(Exception(f"*** Unknown opcode: {self.mem[self.IP]} ***"))
 
         # print("[END]\n")
         return 0
@@ -84,9 +95,9 @@ with open("input.txt","r") as f:
     instructions = [int(i.strip()) for i in f.read().split(',')]
 
 # Part 1:
-Machine(instructions).run(1)
+VM(instructions).run(1)
 # 2494485073
 
 # Part 2:
-Machine(instructions).run(2)
+VM(instructions).run(2)
 # 44997
