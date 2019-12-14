3b7d43dfcc2527d437dcb33b45e1a250be331d34
Dave <dave@unknowngenius.com>
2019-12-14 11:12:19 +0100

    Moved to python package

--- 2019/02/__main__.py
+++ 2019/02/__main__.py
@@ -1,7 +1,31 @@
+from ..utils import inputFile
+
+with open(inputFile()) as f:
+    d = [int(n) for n in f.read().split(',')]
+
+d[1] = 12
+d[2] = 2
+ip = 0
+while d[ip] != 99:
+    if d[ip] == 1:
+        val = d[d[ip+1]] + d[d[ip+2]]
+    elif d[ip] == 2:
+        val = d[d[ip+1]] * d[d[ip+2]]
+    else:
+        print("error")
+        break
+    d[d[ip+3]] = val
+    ip += 4
+
+print(f"Part 1 - Final state d[0]: {d[0]}")
+# 5098658
+
+# Part 2:
+
 def search(target):
     for noun in range(0, 100):
         for verb in range(0, 100):
-            with open('input.txt') as f:
+            with open(inputFile()) as f:
                 d = [int(n) for n in f.read().split(',')]
             d[1] = noun
             d[2] = verb
@@ -21,6 +45,6 @@ def search(target):
 
 res = search(19690720)
 if res:
-    print(f"{res} -> {res[0] * 100 + res[1]}")
+    print(f"Part 2 - {res} -> {res[0] * 100 + res[1]}")
 
 # 5098658
