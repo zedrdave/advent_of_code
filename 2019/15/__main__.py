d5dc3b42108c53c2f6acdb4c85d537b874549ccd
Dave <dave@unknowngenius.com>
2019-12-15 16:50:38 +0100

    moved graphics to dedicated package

--- 2019/15/__main__.py
+++ 2019/15/__main__.py
@@ -3,14 +3,15 @@ import copy
 from collections import defaultdict
 
 from ..intcode.VM import VM, NeedInputException
-from ..utils import loadCSVInput, dprint, asciiPrint, setVerbosity, sparseToDense, getChar, cmp
+from ..utils import loadCSVInput, dprint, setVerbosity
+from ..graphics import snapshot, saveAnimatedGIF
 
 # Debug:
 setVerbosity(False)
 
 # Options:
-print_to_screen = False
-save_animation = True
+printToScreen = False
+saveAnimation = True
 
 instructions = loadCSVInput()
 
@@ -22,7 +23,6 @@ WALL = 2
 OXYGEN = 5
 ROBOT = 6
 
-DIR_NAMES = "NSWE"
 DIRS = [(0,-1),(0,1),(-1,0),(1,0)]
 move = lambda pos, dir: (pos[0]+dir[0], pos[1]+dir[1])
 
@@ -42,31 +42,17 @@ def path_to(orig, dest, wallmap, prev_pos = None):
 cur_pos = (0,0)
 wallmap = defaultdict(int)
 wallmap[cur_pos] = EMPTY
-explore = [(-1,0),(1,0),(0,1),(0,-1)]
-
-frames = []
-def printAndStore(wallmap, update = {}):
-    if not print_to_screen and not save_animation:
-        return
-    printmap = copy.copy(wallmap)
-    printmap.update(update)
-    if save_animation:
-        global frames
-        frames += [printmap]
-    if print_to_screen:
-        asciiPrint(printmap, transpose = True)
-
+explore = [(1,0)]
 
 while len(explore) > 0:
     next_dest = explore.pop()
     next_path = path_to(cur_pos, next_dest, wallmap)
-    dprint(f"Cur pos: {cur_pos}\nNext dest: {next_dest}\nPath: {[DIR_NAMES[d] for d in next_path]}")
-    vm.input = next_path
+    dprint(f"Cur pos: {cur_pos}\nNext dest: {next_dest}\nPath: {['NSWE'[d] for d in next_path]}")
     for dir_idx in next_path:
         vm.input = [dir_idx+1]
         status = next(vm.run())
         next_pos = move(cur_pos, DIRS[dir_idx])
-        dprint(f"Moved: {DIR_NAMES[dir_idx]} -> {next_pos}({status})")
+        dprint(f"Moved: {'NSWE'[dir_idx]} -> {next_pos}({status})")
 
         if status == 0:
             wallmap[next_pos] = WALL
@@ -74,19 +60,16 @@ while len(explore) > 0:
             cur_pos = next_pos
 
             if wallmap[cur_pos] == UNKNOWN:
-                for dir in DIRS:
-                    if wallmap[move(cur_pos, dir)] == UNKNOWN:
-                        explore += [move(cur_pos, dir)]
-
+                explore += [p for p in [move(cur_pos, dir) for dir in DIRS] if wallmap[p] == UNKNOWN]
             if status == 2:
-                wallmap[next_pos] = OXYGEN
-                path = path_to((0,0), next_pos, wallmap)
-                dprint("Found oxygen at: ", next_pos, " | path: ", path)
+                wallmap[cur_pos] = OXYGEN
+                path = path_to((0,0), cur_pos, wallmap)
+                dprint("Found oxygen at: ", cur_pos, " | path: ", path)
                 print("Part 1 -", len(path))
             else:
-                wallmap[next_pos] = EMPTY
+                wallmap[cur_pos] = EMPTY
 
-        printAndStore(wallmap, {cur_pos: ROBOT})
+        snapshot(wallmap, printToScreen, saveAnimation, {cur_pos: ROBOT})
 
 # Part 2
 
@@ -97,34 +80,9 @@ while time == 0 or len(empty_neighbours) > 0:
     for p in empty_neighbours:
         wallmap[p] = OXYGEN
     time += 1
-    printAndStore(wallmap)
+    snapshot(wallmap, printToScreen, saveAnimation)
 
 print("Part 2: took ", time)
-# Visualisation: print animation
 
-if len(frames):
-    print(f"Saving animation ({len(frames)} frames)…")
-
-    from PIL import Image
-
-    tileSize = 10
-    numTiles = 41
-    tiles = [Image.open(f'2019/15/tiles/{i}.png').resize((tileSize,)*2) for i in range(7)]
-
-    def frameToImage(frame, i):
-        img = Image.new('RGBA', (numTiles*tileSize,)*2, 'black')
-        for (x,y),v in frame.items():
-            img.paste(tiles[v], ((y + numTiles//2 + 1)*tileSize, (x + numTiles//2 + 1)*tileSize))
-        global saved
-        print(f"saved: {i}/{len(frames)}", end="\r")
-        return img
-
-    images = (frameToImage(f,i) for i,f in enumerate(frames))
-    next(images).save(
-        'animation.gif',
-        save_all=True,
-        append_images=images,
-        duration=10,
-        loop=0,
-        optimize=True
-    )
+if saveAnimation:
+    saveAnimatedGIF(freq=10)
---
+++ 2019/graphics/__init__.py
@@ -0,0 +1,60 @@
+import sys
+import copy
+import numpy as np
+from PIL import Image
+from collections import defaultdict
+
+from ..utils import sparseToDense
+
+frames = []
+
+def snapshot(sparseDict, printToScreen = True, saveAnimation = False, update = {}):
+    if not printToScreen and not saveAnimation:
+        return
+    frame = copy.copy(sparseDict)
+    frame.update(update)
+    if saveAnimation:
+        global frames
+        frames += [frame]
+    if printToScreen:
+        asciiPrint(frame, transpose = True)
+
+
+def asciiPrint(bitmap, transpose=False, reset=False, header=""):
+    if type(bitmap) is defaultdict:
+        bitmap = sparseToDense(bitmap)
+    else:
+        bitmap = np.array(bitmap)
+    if transpose:
+        bitmap = bitmap.transpose()
+    if bitmap.shape[0] > 500 or bitmap.shape[1] > 500:
+        raise(Exception(f"Bitmap too large to be printed: {np.array(bitmap).shape}"))
+    if reset:
+        print(chr(27) + "[2J", flush=False)
+    print(header, flush=False)
+    print("\n".join(''.join([u"⬛️",u"⬜️",u"🟥",u"🟨", u"🔵",u"🟦",u"🤖"][int(i)] for i in line) for line in bitmap), flush=False)
+    sys.stdout.flush()
+
+def saveAnimatedGIF(numTiles = 41, tileSize = 10, outputFile = 'animation.gif', freq = 1):
+    global frames
+
+    print(f"Saving animation ({len(frames)} frames)…")
+    tiles = [Image.open(f'2019/15/tiles/{i}.png').resize((tileSize,)*2) for i in range(7)]
+
+    def frameToImage(frame, i):
+        img = Image.new('RGBA', (numTiles*tileSize,)*2, 'black')
+        for (x,y),v in frame.items():
+            img.paste(tiles[v], ((y + numTiles//2 + 1)*tileSize, (x + numTiles//2 + 1)*tileSize))
+        global saved
+        print(f"saved: {i}/{len(frames)}", end="\r")
+        return img
+
+    images = (frameToImage(f,i) for i,f in enumerate(frames) if i%freq == 0)
+    next(images).save(
+        outputFile,
+        save_all=True,
+        append_images=images,
+        duration=10,
+        loop=0,
+        optimize=True
+    )
