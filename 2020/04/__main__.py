c6edf6286de92102c8c52fa91f82976e9ab6db66
Dave <dave@tenko.ai>
2020-12-04 07:34:50 +0100

    variants using partial and try/except

--- 2020/04/__main__.py
+++ 2020/04/__main__.py
@@ -16,7 +16,7 @@ validators = {
     'hgt': lambda x: (x.endswith('cm') and 150 <= int(x[:-2]) <= 193) or (x.endswith('in') and 59 <= int(x[:-2]) <= 76),
     'hcl': lambda x: re.match('^#[0-9a-f]{6}$', x),
     'ecl': lambda x: x in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'],
-    'pid': lambda x: re.match('^\d{9,9}$', x),
+    'pid': lambda x: re.match('^\d{9}$', x),
     'cid': lambda x: True,
 }
 
@@ -27,6 +27,31 @@ ans2 = sum([all(validator(p.get(k, '')) for k,validator in validators.items()) f
 print(f"Part 1: {ans1}\nPart 2: {ans2}")
 
 
+###############################################
+#
+# Variant with functools.partial:
+#
+###############################################
+
+from functools import partial
+
+def check_year(f, t, y):
+    return re.match('^\d{4}$', y) and f <= int(y) <= t
+
+validators = {
+    'byr': partial(check_year, 1920, 2002),
+    'iyr': partial(check_year, 2010, 2020),
+    'eyr': partial(check_year, 2020, 2030),
+    'hgt': lambda x: (x.endswith('cm') and 150 <= int(x[:-2]) <= 193) or (x.endswith('in') and 59 <= int(x[:-2]) <= 76),
+    'hcl': partial(re.match, '^#[0-9a-f]{6}$'),
+    'ecl': lambda x: x in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'],
+    'pid': partial(re.match, '^\d{9}$'),
+    'cid': lambda x: True,
+}
+
+ans2 = sum([all(validator(p.get(k, '')) for k,validator in validators.items()) for p in passports])
+
+print(f"Part 2 (using functools.partial): {ans2}")
 
 ###############################################
 #
@@ -41,3 +66,5 @@ for p in passports:
             ans2 += 1
     except:
         print('Invalid format:', p)
+
+print(f"Part 2 (with exception catching): {ans2}")
