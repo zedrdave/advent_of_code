9e6ccc328721f21891cd66edf80d9d382ab84d95
zedrdave <>
2020-12-08 15:01:25 +0100

    handling edge cases

--- 2020/08/__main__.py
+++ 2020/08/__main__.py
@@ -2,18 +2,21 @@ from ..utils import input_file
 
 𝓟 = [(l[:3], int(l[4:])) for l in open(input_file())]
 
-def run(P):
-    𝒜, IP = 0, 0
+def run(P, IP = 0):
+    𝒜 = 0
     ℮ = set()
-    while IP < len(P) and IP not in ℮:
+    while 0 <= IP < len(P):
         ℮.add(IP)
         𝒾, a = P[IP]
         if 𝒾 == 'jmp':
             IP += a-1
         if 𝒾 == 'acc':
             𝒜 += a
-        IP+=1
-    return IP not in ℮, 𝒜
+        IP += 1
+        if IP in ℮:
+            return False, 𝒜
+
+    return IP == len(P), 𝒜
 
 print('Part 1', run(𝓟)[1])
 
@@ -22,7 +25,7 @@ for l,(𝒾,a) in enumerate(𝓟):
         𝒾 = 'nop'
     elif 𝒾 == 'nop':
         𝒾 = 'jmp'
-    b,𝒜=run(𝓟[:l] + [(𝒾,a)] + 𝓟[l+1:])
+    b,𝒜 = run(𝓟[:l] + [(𝒾,a)] + 𝓟[l+1:])
     if b:
         break
 
