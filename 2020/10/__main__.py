3676232e252c86678e2f00e12388a23db4e62022
zedrdave <dave@tenko.ai>
2020-12-10 19:27:44 +0000

    day 10 - using memoize

--- 2020/10/__main__.py
+++ 2020/10/__main__.py
@@ -1,6 +1,11 @@
 from ..utils import *
 
+########################################
+#
 # Solution using Tribonacci sequence
+#
+########################################
+
 D = sorted(int(i) for i in open(input_file()))
 
 δ = [i-j for i,j in zip(D, [0]+D)] + [3]
@@ -15,3 +20,29 @@ while len(T) <= max(L): T += [sum(T[-3:])]
 import math
 print('Part 1:', (len(δ)-len(Δ))*len(Δ),
       '\nPart 2:', math.prod(T[l] for l in L))
+
+# 94.1 µs ± 2.24 µs per loop (mean ± std. dev. of 7 runs, 10000 loops each)
+
+
+########################################
+#
+# Using good ol' memoization
+#
+########################################
+
+D = sorted(int(i) for i in open(input_file()))
+D = [0, *D, D[-1]+3]
+
+from collections import Counter
+C = Counter(j-i for i,j in zip(D,D[1:]))
+
+from functools import lru_cache
+@lru_cache
+def combos(A):
+    if len(A) == 2: return 1
+    return combos(A[1:]) + (combos(A[0:1]+A[2:]) if A[2]-A[0] < 4 else 0)
+
+
+print('Part 1:', C[3]*C[1], '\nPart 2:', combos(tuple(D)))
+
+# 328 µs ± 4.94 µs per loop (mean ± std. dev. of 7 runs, 1000 loops each)
