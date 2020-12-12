e1688c51e34954060fa5925098ebc942d76fa83b
zedrdave <dave@tenko.ai>
2020-12-12 11:45:44 +0000

    day 12, part 1&2 factorised

--- 2020/12/__main__.py
+++ 2020/12/__main__.py
@@ -1,30 +1,30 @@
 from ..utils import *
 
-P,W = [0,0],[1,0]
+ℳ = lambda x,y,dx,dy,d: (x+d*dx, y+d*dy)
 
-ℳ = lambda x,y,dx,dy,d: (x+d*dx, y+d*dy) # Move
+cos = [1,0,-1,0]
+sin = [0,1,0,-1]
+ℛ = lambda x,y,t: (x*cos[t]-y*sin[t], x*sin[t]+y*cos[t])
 
-cos, sin = [1,0,-1,0], [0,1,0,-1] # by increments of 90
-ℛ = lambda x,y,t: (x*cos[t]-y*sin[t], x*sin[t]+y*cos[t]) # Rotate
+def s(W=[1,0], pt=1):
+    P = [0,0]
 
-for i in open(input_file()):
-    c,n = i[0], int(i[1:])
+    for i in open(input_file()):
+        c,n= i[0],int(i[1:])
+        if c == 'F':
+            P = ℳ(*P,*W,n)
+        elif c == 'R':
+            W = ℛ(*W,-n//90%4)
+        elif c == 'L':
+            W = ℛ(*W,n//90)
+        else:
+            r = ℛ(1,0,'ENWS'.index(c))
+            if pt-1:
+                W = ℳ(*W, *r, n)
+            else:
+                P = ℳ(*P, *r, n)
 
-    if c == 'F': P = ℳ(*P, *W, n)
-    elif c == 'R': W = ℛ(*W, -n//90 % 4)
-    elif c == 'L': W = ℛ(*W, n//90)
-    else: P = ℳ(*P, *ℛ(1, 0, 'ENWS'.index(c)), n)
+    print(f'Part {pt}:', abs(P[0]) + abs(P[1]))
 
-print('Part 1:', abs(P[0]) + abs(P[1]))
-
-P,W = [0,0],[10,1]
-
-for i in open(input_file()):
-    c,n = i[0], int(i[1:])
-
-    if c == 'F': P = ℳ(*P, *W, n)
-    elif c == 'R': W = ℛ(*W, -n//90 % 4)
-    elif c == 'L': W = ℛ(*W, n//90)
-    else: W = ℳ(*W, *ℛ(1, 0, 'ENWS'.index(c)), n)
-
-print('Part 2:', abs(P[0]) + abs(P[1]))
+s()
+s([10,1],2)
