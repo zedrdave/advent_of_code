f6fd188c4d260c754aa0f2c8f315aca68913a931
zedrdave <dave@tenko.ai>
2021-12-03 12:38:29 +0000

    part 2 rust

--- 2021/03/rust/src/main.rs
+++ 2021/03/rust/src/main.rs
@@ -26,6 +26,28 @@ fn least_most(sums: &Vec<u16>, data_len: u16, least: bool) -> u32 {
         })
 }
 
+fn sieve(data: &Vec<Vec<u8>>, least: bool) -> u32 {
+    let mut candidates: Vec<Vec<u8>> = data.clone();
+
+    for i in 0..candidates[0].len() {
+        let sum: u32 = candidates.iter().fold(0, |accum, v| (accum + v[i] as u32));
+        let bit_criteria = (2 * sum < (candidates.len() as u32)) as bool ^ least;
+
+        candidates = candidates
+            .into_iter()
+            .filter(|v| (v[i] != 0) == bit_criteria)
+            .collect();
+
+        if candidates.len() == 1 {
+            break;
+        };
+    }
+
+    candidates[0]
+        .iter()
+        .fold(0, |accum, b| accum * 2 + (*b as u32))
+}
+
 fn main() {
     let data = read_file("../input.txt");
     let sums: Vec<u16> = data.iter().fold(vec![0; data[0].len()], |mut accum, v| {
@@ -39,4 +61,5 @@ fn main() {
     let least = least_most(&sums, data.len() as u16, true);
 
     println!("Part 1: {}", (most * least));
+    println!("Part 2: {}", sieve(&data, false) * sieve(&data, true));
 }
