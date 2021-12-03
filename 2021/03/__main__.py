e82740de47a0f934d82b5b2e16240e675458c954
zedrdave <dave@tenko.ai>
2021-12-03 11:34:12 +0000

    optim part 1 python

--- 2021/03/__main__.py
+++ 2021/03/__main__.py
@@ -3,26 +3,26 @@ data = load_int_input(3)
 
 data = data.split('\n')
 
-tots = [sum(1 for l in data if l[i] == '1') for i in range(len(data[0]))]
+bits = [sum(1 for l in data if l[i] == '1') >
+        len(data)/2 for i in range(len(data[0]))]
 
 
-def fn(cmp): return int(''.join(str(int(cmp(i))) for i in tots), 2)
+def count(most): return sum((b ^ most) * 2 **
+                            i for (i, b) in enumerate(reversed(bits)))
 
 
-print('Part 1: ', fn(lambda x: x >= len(data)/2)
-      * fn(lambda x: x < len(data)/2))
-
-arr = data
+print('Part 1:', count(True)*count(False))
 
 
-def sieve(arr, fn):
+def sieve(most):
+    arr = data
     for i in range(len(arr[0])):
-        tot = sum(1 for l in arr if l[i] == '1')
-        bit_criteria = str(int(tot < len(arr)/2))
-        arr = [l for l in arr if l[i] == bit_criteria]
+        bit_criteria = (
+            sum(1 for l in arr if l[i] == '1') >= len(arr)/2) ^ most
+        arr = [l for l in arr if int(l[i]) == bit_criteria]
+
         if len(arr) == 1:
             return int(arr[0], 2)
 
 
-print('Part 2: ', sieve(data, lambda tot, arr: tot <= len(arr)/2)
-      * sieve(data, lambda tot, arr: tot >= len(arr)/2))
+print('Part 2: ', sieve(True) * sieve(False))
