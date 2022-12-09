42c024134f6109a1bf283229947c9efaea69bcc3
Dave <dave@tenko.ai>
2022-12-09 11:34:16 +0000

    day 9

--- 2022/09.py
+++ 2022/09.py
@@ -12,18 +12,39 @@ def solve(data, num_knots=10):
     K = [(0, 0)] * num_knots
     visited = set(K)
 
-    for l in data.split("\n"):
-        dir, steps = l.split()
-        for _ in range(int(steps)):
-            K[0] = move(K[0], dirs[dir])
-            for k in range(1, len(K)):
-                delta = [K[k-1][i] - K[k][i] for i in (0, 1)]
-                if abs(max(delta, key=abs)) > 1:
-                    K[k] = move(K[k], [x//(abs(x) or 1) for x in delta])
-            visited.add(K[-1])
+    moves = [l[0] for l in data.split("\n") for _ in range(int(l.split()[1]))]
+
+    for dir in moves:
+        K[0] = move(K[0], dirs[dir])
+        for k in range(1, len(K)):
+            delta = [K[k-1][i] - K[k][i] for i in (0, 1)]
+            if abs(max(delta, key=abs)) > 1:
+                K[k] = move(K[k], [(x > 0)-(x < 0) for x in delta])
+        visited.add(K[-1])
 
     return len(visited)
 
 
-print("Part 1:", solve(data, 2))
-print("Part 2:", solve(data, 10))
+solve(data, 2), solve(data, 10)
+
+
+# Compact version:
+
+# move = lambda X,d: (X[0]+d[0], X[1]+d[1])
+# delta = lambda X,i: move(X[i-1], [-x for x in X[i]])
+# absmax = lambda X: abs(max(X, key=abs))
+# def solve(data, num_knots = 10):
+#     K = [(0,0)] * num_knots
+#     visited = set(K)
+
+#     move_dirs = [dirs[l[0]] for l in data.split("\n") for _ in range(int(l.split()[1]))]
+
+#     for dir in move_dirs:
+#         K[0] = move(K[0], dir)
+#         K[1:] = [move(K[k], [(x>0)-(x<0) for x in delta(K, k)])
+#                     if absmax(delta(K, k)) > 1
+#                     else K[k]
+#                 for k in range(1, len(K))]
+#         visited.add(K[-1])
+
+#     return len(visited)
