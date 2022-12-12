9164aead9be3d56d324dcc55b612df4bb350da88
Dave <dave@tenko.ai>
2022-12-12 15:25:49 +0000

    day 11

--- 2022/12.py
+++ 2022/12.py
@@ -4,9 +4,7 @@ import sys
 
 day = 12
 year = 2022
-
 data = get_data(day=day, year=year)
-
 
 grid = {(i, j): ord(c) - ord('a')
         for j, l in enumerate(data.split('\n')) for i, c in enumerate(l)}
@@ -14,22 +12,18 @@ end, start = [next(k for k, v in grid.items() if v == i) for i in (-28, -14)]
 grid[end] = 25
 grid[start] = 0
 
-
 add = lambda x, d: (x[0] + d[0], x[1] + d[1])
 
-
 dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-stack = [end]
 dists = defaultdict(lambda: sys.maxsize)
 dists[end] = 0
+stack = [end]
 
 while len(stack):
     coords = stack.pop()
     for dir in dirs:
         neigh = add(coords, dir)
-        if neigh not in grid:
-            continue
-        if (dists[neigh] > dists[coords] + 1) and (grid[neigh] >= grid[coords] - 1):
+        if (neigh in grid) and (dists[neigh] > dists[coords] + 1) and (grid[neigh] >= grid[coords] - 1):
             dists[neigh] = dists[coords] + 1
             stack.append(neigh)
 
